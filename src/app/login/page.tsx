e4a8944fee8643247b57fc17fb7b473fe5a1dd08
TH-Raju <rajukhan894200@gmail.com>
2024-02-18 03:24:35 +0600

    input created for login form

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,56 @@
+"use client";
+
+import { Button, Col, Row } from "antd";
+import loginImage from "../../assets/login.png";
+import Image from "next/image";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/Forminput";
+import { SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  id: string;
+  password: string;
+};
+
+const onSubmit: SubmitHandler<FormValues> = (data) => {
+  try {
+    console.log(data);
+  } catch (e) {}
+};
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1>This is Login Page</h1>
-    </div>
+    <Row>
+      <Col sm={12} md={16} lg={16}>
+        <Image src={loginImage} width={500} alt="login" />
+      </Col>
+      <Col sm={12} md={8} lg={8}>
+        <h1>First login your account</h1>
+        <div>
+          <Form submitHandler={onSubmit}>
+            <div>
+              <FormInput
+                name="id"
+                type="text"
+                size="large"
+                label="User ID"
+              ></FormInput>
+            </div>
+            <div>
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="User Password"
+              ></FormInput>
+            </div>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
